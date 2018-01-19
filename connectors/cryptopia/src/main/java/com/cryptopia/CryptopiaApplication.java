ccc41e317dcb5659bb2c88e04c4078906ac84f3a
manel <arvak7@gmail.com>
2018-01-19 20:44:25 +0100

    Initial commit

---
+++ connectors/cryptopia/src/main/java/com/cryptopia/CryptopiaApplication.java
@@ -0,0 +1,12 @@
+package com.cryptopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptopiaApplication.class, args);
+	}
+}
---
+++ connectors/cryptopia/src/test/java/com/cryptopia/IntegrationTest.java
@@ -0,0 +1,14 @@
+package com.cryptopia;
+
+import org.junit.Test;
+
+/**
+ * Created by manel on 19/01/18.
+ */
+public class IntegrationTest {
+
+    @Test
+    public void tesat() {
+
+    }
+}
